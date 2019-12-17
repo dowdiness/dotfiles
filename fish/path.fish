46146c4724469d5df44ba2834620dfcc1f58f32e
koji Ishimoto <koji.programs@gmail.com>
2019-12-17 21:04:40 +0900

    Fix pyenv/pyenv/issues/688

--- fish/path.fish
+++ fish/path.fish
@@ -4,3 +4,6 @@ set -x PATH ~/.composer/vendor/bin $PATH
 set -x PATH ~/.local/bin $PATH
 # MongoDB path
 set -x PATH /Users/Koji/development/mongodb/bin $PATH
+# please refer to this issue https://github.com/pyenv/pyenv/issues/688
+set -x GIT_INTERNAL_GETTEXT_TEST_FALLBACKS 1
+
