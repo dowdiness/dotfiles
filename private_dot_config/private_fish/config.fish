9308f67823ee856fc2837ebc2025c72add62ba06
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-06-16 18:51:01 +0900

    update dotfiles

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,47 @@
+#view
+set -g theme_display_date yes
+set -g theme_date_format "+%F %H:%M"
+set -g theme_display_git_default_branch yes
+set -g theme_color_scheme dark
+
+if status is-interactive
+    eval (/opt/homebrew/bin/brew shellenv)
+    source "$HOME/ghq/github.com/dowdiness/dotfiles/fish/common.fish"
+    source "$HOME/ghq/github.com/dowdiness/dotfiles/fish/path.fish"
+    # initialize rbenv
+    rbenv init - fish | source
+
+    # initialise nodenv
+    nodenv init - fish | source
+
+    # initialize pyenv
+    if which pyenv-virtualenv-init > /dev/null
+        pyenv init - fish | source
+    end
+
+    # initialize jenv
+    set PATH $HOME/.jenv/bin $PATH
+    jenv init - fish | source
+
+    # To install useful key bindings and fuzzy completion:
+    # (brew --prefix) /opt/fzf/install
+
+    # path for postgresql
+    set -x PGDATA /usr/local/var/postgres/
+
+    # path for hub
+    set -x BROWSER open
+
+    # https://github.com/starship/starship init
+    eval (starship init fish)
+
+    # The next line updates PATH for the Google Cloud SDK.
+    if [ -f '/Users/AntiSatori/google-cloud-sdk/path.fish.inc' ]; . '/Users/AntiSatori/google-cloud-sdk/path.fish.inc'; end
+    set -gx VOLTA_HOME "$HOME/.volta"
+    set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+
+    # https://noknow.info/it/shell_script/tips/curl_ssl_certificate_probrem_60?lang=ja
+    set -x CURL_CA_BUNDLE /usr/local/share/curl/cacert.pem
+    set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/AntiSatori/.ghcup/bin $PATH # ghcup-env
+end
---
+++ private_dot_config/private_fish/path.fish
@@ -0,0 +1,12 @@
+# Laravel path
+set -x PATH ~/.composer/vendor/bin $PATH
+# AWS path
+set -x PATH ~/.local/bin $PATH
+# MongoDB path
+set -x PATH /Users/Koji/development/mongodb/bin $PATH
+# Rust Cargo path
+set -x PATH ~/.cargo/bin $PATH
+# Self-made commands path
+set -x PATH ~/bin $PATH
+# please refer to this issue https://github.com/pyenv/pyenv/issues/688
+set -x GIT_INTERNAL_GETTEXT_TEST_FALLBACKS 1
